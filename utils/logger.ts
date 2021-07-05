65e080a5a90e416a2448d0f2a87ba38b5ab8b326
JosiasAurel <ndjosiasaurel@gmail.com>
2021-07-05 15:03:56 +0100

    fix logger type error to match apollo Logger type

--- utils/logger.ts
+++ utils/logger.ts
@@ -1,4 +1,20 @@
 
-const requestLogger: Function = (message: string) => console.log(`[CropBoard:Server ⚡🚀 ] -> Log | ${message}`);
+import { Logger } from "apollo-server-types";
+
+interface CBLogger {
+    debug: string
+    info: string
+    warn: string
+    error: string
+}
+
+const logHandler: Function = (message: string): void => console.log(`[CropBoard:Server ⚡🚀 ] -> Log | ${message}`);
+
+const requestLogger: Logger = {
+    debug: (msg: string) => logHandler(msg),
+    info: (msg: string) => logHandler(msg),
+    warn: (msg: string) => logHandler(msg),
+    error: (msg: string) => logHandler(msg)
+}
 
 export { requestLogger };
