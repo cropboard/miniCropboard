4e730a8c418a8da4b3abba519b063a8ee440abe2
JosiasAurel <ndjosiasaurel@gmail.com>
2021-07-06 13:03:22 +0100

    wipe dummy data and user new typedefs and resolvers

--- index.ts
+++ index.ts
@@ -1,73 +1,13 @@
 
 import { ApolloServer, gql } from "apollo-server";
 import { requestLogger } from "./utils/logger";
-import { Farm, User } from "./types";
+import { typeDefs } from "./graphql/typeDefs";
+import { resolvers } from "./graphql/resolvers";
 
-// dataset
-let users: Array<User> = [
-    {
-        name: "Dane",
-        id: 1
-    },
-    {
-        name: "Mike",
-        id: 2
-    },
-    {
-        name: "Kyle",
-        id: 3
-    }
-]
-
-let farms: Array<Farm> = [
-    {
-        title: "Sample1",
-        user: 2
-    },
-    {
-        title: "MyFarm",
-        user: 1
-    },
-    {
-        title: "Farm001",
-        user: 3
-    }
-]
-
-
-const typeDefs = gql`
-
-    type Farm {
-        title: String!
-        user: User
-    }
-
-    type User {
-        name: String!
-        id: ID!
-        farms: [Farm]
-    }
-
-    type Query {
-        users: [User]
-        farms: [Farm]
-        user(id: ID): User
-    }
-`
-
-const resolvers = {
-    Query: {
-        users: () => users,
-        farms: () => farms,
-        user: (parent: any, args: any) => users.find(user => user.id === args.id)
-    },
-    Farm: {
-        user: (parent: Farm, args: any) => {
-            return users.find(user => user.id === parent.user)
-        }
-    }
-}
-
+/* 
+Instance the server with type definitions
+and resolvers
+*/
 
 const server = new ApolloServer({
     typeDefs, 
