225a52b2626c48947a3685ce7af6e9c94fcc8482
JosiasAurel <ndjosiasaurel@gmail.com>
2021-07-06 16:30:20 +0100

    make clearer weather interface

--- database/crop.ts
+++ database/crop.ts
@@ -3,10 +3,25 @@ import { Schema, Document, model } from "mongoose";
 // naturals
 enum CropCategory { Legume, Fruit, Vegetable }
 
+// all of the weather data below are atmoshperic
+/* 
+Humidity defaults to %
+temperature defaults to °C
+visibility round field 8.0km
+we will get dew point from 
+*/
+interface WeatherData {
+    speed: string
+    pressure: string
+    humidity: number
+    UV: number
+    atmosphericTemperature: number
+}
+
 interface CBCrop extends Document {
     name: string
     category: CropCategory
-    weather: object
+    weather: WeatherData
     fertilizer: string
     water: string
     cost: string
