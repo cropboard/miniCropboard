c43aaae8235e57969e30db47748055191941b666
JosiasAurel <ndjosiasaurel@gmail.com>
2021-07-06 12:07:40 +0100

    create server serviceconfig

---
+++ serviceConfig.ts
@@ -0,0 +1,6 @@
+
+const mongoDBURI = "mongodb://locallhost:27017/cropboard";
+
+const mongoDBAccessConfig = { useNewUrlParser: true, useUnifiedTopology: true };
+
+export { mongoDBURI, mongoDBAccessConfig };
