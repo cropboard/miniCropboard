b5f91e252618c68eda453598a957a7f782d43a6e
JosiasAurel <ndjosiasaurel@gmail.com>
2021-07-06 14:40:13 +0100

    correct mongoDB local URI

--- serviceConfig.ts
+++ serviceConfig.ts
@@ -1,5 +1,5 @@
 
-const mongoDBURI = "mongodb://locallhost:27017/cropboard";
+const mongoDBURI = "mongodb://localhost:27017/cropboard";
 
 const mongoDBAccessConfig = { useNewUrlParser: true, useUnifiedTopology: true };
 
