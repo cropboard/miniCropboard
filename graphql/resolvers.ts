0026e3c0f5598ca6d35b82b6d118fb9b622934a6
JosiasAurel <ndjosiasaurel@gmail.com>
2021-07-06 14:39:14 +0100

    fix creating user to return credentials

--- graphql/resolvers.ts
+++ graphql/resolvers.ts
@@ -11,8 +11,43 @@ const resolvers = {
         user: (parent: any, args: any) => {
             const user = User.findById(args.id, (error: any, foundUser: any) => {
                 if (error) return Error(error);
+                console.log(foundUser);
                 return foundUser;
             });
+        }
+    },
+    Mutation: {
+        createUser: (parent: any, args: any) => {
+            let isError: boolean = false;
+            const newUser = User.create({
+                name: args.name,
+                email: args.email,
+                password: args.password,
+                registrationDate: args.registraionDate,
+                location: args.location
+            }, (error: any, newuser) => {
+                // handle your fail safes bro ;)
+
+                if (error) {
+                    isError = true;
+                    return Error(error);
+                };
+                // console.log(newuser);
+                return newuser;
+            });
+           if (!isError) {
+               return { // return the info of the newly created user
+                name: args.name,
+                email: args.email,
+                location: args.location
+               }
+           } else {
+               return { // return the info of the newly created user
+                name: "NO",
+                email: "NO",
+                location: "NO"
+               }
+           }
         }
     }
 }
