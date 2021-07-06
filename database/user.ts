2beed677a40ab31fa1eff06a9b122b455004159d
JosiasAurel <ndjosiasaurel@gmail.com>
2021-07-06 12:07:25 +0100

    create user schema

---
+++ database/user.ts
@@ -0,0 +1,22 @@
+
+import { Schema, Document, model } from "mongoose";
+
+interface CBUser extends Document {
+    name: string
+    email: string
+    password: string
+    registrationDate: string
+    location: string
+}
+
+const CBUserSchema = new Schema<CBUser>({
+    name: String,
+    email: String,
+    password: String,
+    registrationDate: String,
+    location: String
+});
+
+const User = model("User", CBUserSchema);
+
+export default User;
