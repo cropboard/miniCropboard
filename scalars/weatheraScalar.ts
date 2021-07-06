d38402fb82e4c077e9c1ed6432a26a3ebcd9e127
JosiasAurel <ndjosiasaurel@gmail.com>
2021-07-06 18:15:32 +0100

    create custom weather scalar

---
+++ scalars/weatheraScalar.ts
@@ -0,0 +1,22 @@
+
+import { GraphQLScalarType, Kind, parseValue } from "graphql";
+
+const weatherScalar = new GraphQLScalarType({
+    name: "Weather",
+    description: "Weather scalar to represent weather data",
+    serialize(value) {
+        return JSON.parse(value)
+    },
+    parseValue(value) {
+        return JSON.parse(value)
+    },
+    parseLiteral(ast) {
+        if (ast.kind === Kind.STRING) {
+            return JSON.stringify(ast.value)
+        }
+
+        return null
+    }
+});
+
+export { weatherScalar };
