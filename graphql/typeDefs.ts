00d0c39c9c1a32b7776e4c7cc60856906d364b05
JosiasAurel <ndjosiasaurel@gmail.com>
2021-07-06 15:16:38 +0100

    add updateFarm mutation structure

--- graphql/typeDefs.ts
+++ graphql/typeDefs.ts
@@ -42,6 +42,8 @@ type Query {
 
 type Mutation {
     createUser(name: String!, email: String!, password: String!, registrationDate: String!, location: String!): User
+    createFarm(title: String!, owner: String!, location: String!, fertilizer: String!, inputSeeds: String!, plant: String!, category: String!): Farm
+    updateFarm(title: String, location: String, fertilizer: String, inputSeeds: String, plant: String, category: String): Farm
 }
 
 `;
