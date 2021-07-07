b4106605201ee4009fbf69d467dcf1e6f1a9621e
JosiasAurel <ndjosiasaurel@gmail.com>
2021-07-07 19:15:20 +0100

    mock dashboard header

---
+++ client/components/dashboard/Header.tsx
@@ -0,0 +1,11 @@
+import React, { FunctionComponent } from "react";
+
+const DashboardHeader: FunctionComponent = (): JSX.Element => {
+    return (
+        <div>
+            <h1>C</h1>
+        </div>
+    )
+}
+
+export default DashboardHeader;
