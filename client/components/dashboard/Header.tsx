ad650ab34238701d2f0f797c194483ca64c25d85
JosiasAurel <ndjosiasaurel@gmail.com>
2021-07-07 19:23:51 +0100

    include logo in header

--- client/components/dashboard/Header.tsx
+++ client/components/dashboard/Header.tsx
@@ -1,9 +1,14 @@
 import React, { FunctionComponent } from "react";
 
+// import logo component
+import Logo from "../logo";
+
 const DashboardHeader: FunctionComponent = (): JSX.Element => {
     return (
         <div>
-            <h1>C</h1>
+            <Logo />
+
+            <h2>Header</h2>
         </div>
     )
 }
