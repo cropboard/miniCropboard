343026299a4dbe62da7b18ba58c7edb5e59cc83f
JosiasAurel <ndjosiasaurel@gmail.com>
2021-07-08 10:37:00 +0100

    sample control button

--- client/components/dashboard/mainControls.tsx
+++ client/components/dashboard/mainControls.tsx
@@ -2,6 +2,7 @@ import React, { FunctionComponent } from "react";
 
 // import mainControl components
 import CreateFarmButton from "./createFarmButton";
+import ControlButton from "./ControlButton";
 
 const DashboardMaincontrols: FunctionComponent = (): JSX.Element => {
     return (
