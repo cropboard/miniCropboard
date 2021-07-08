c0f3e9dd38bef26cf22cbcafdd17a9a6a56ac8ba
JosiasAurel <ndjosiasaurel@gmail.com>
2021-07-08 12:13:01 +0100

    add form in register page

--- client/pages/auth/register.tsx
+++ client/pages/auth/register.tsx
@@ -3,10 +3,21 @@ import React, { FunctionComponent } from "react";
 // import styles 
 import styles from "../../styles/auth/register.module.css";
 
+// import custom components
+import AuthPagesHeader from "../../components/auth/Header";
+
 const RegisterPage: FunctionComponent = (): JSX.Element => {
     return (
         <div>
+            <AuthPagesHeader context="Registration" />
 
+            <form>
+                <input type="text" placeholder="Name" />
+                <input type="text" placeholder="Email" />
+                <input type="text" placeholder="Password" />
+                <input type="text" placeholder="Confirm Password" />
+                <input type="text" placeholder="Location" />
+            </form>
         </div>
     )
 }
