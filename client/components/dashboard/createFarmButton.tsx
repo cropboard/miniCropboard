d71e7da35717ccb5b44482541fe896fa8cd04189
JosiasAurel <ndjosiasaurel@gmail.com>
2021-07-08 09:51:41 +0100

    create farm button component

---
+++ client/components/dashboard/createFarmButton.tsx
@@ -0,0 +1,16 @@
+import React, { FunctionComponent } from "react";
+
+
+
+const CreateFarmButton: FunctionComponent = (): JSX.Element => {
+    return (
+        <div>
+            <h2>
+                New Farm
+            </h2>
+            <img src="/dashboard/plus-outline.svg" alt="" />
+        </div>
+    )
+}
+
+export default CreateFarmButton;
