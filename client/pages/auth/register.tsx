f79e8c9873e25ba595b551d6b0c658f1e3b46fa7
JosiasAurel <ndjosiasaurel@gmail.com>
2021-07-08 12:03:46 +0100

    create set of auth pages

---
+++ client/pages/auth/register.tsx
@@ -0,0 +1,14 @@
+import React, { FunctionComponent } from "react";
+
+// import styles 
+import styles from "../../styles/auth/register.module.css";
+
+const RegisterPage: FunctionComponent = (): JSX.Element => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default RegisterPage;
