fda025d2988f4ba83177b084d96d7794229214b9
JosiasAurel <ndjosiasaurel@gmail.com>
2021-07-08 11:59:47 +0100

    NoFarms if no farms

--- client/pages/dashboard/index.tsx
+++ client/pages/dashboard/index.tsx
@@ -4,13 +4,28 @@ import React, { FunctionComponent } from "react";
 import DashboardHeader from "../../components/dashboard/Header";
 import DashboardMaincontrols from "../../components/dashboard/mainControls";
 
+import NoFarms from "../../components/dashboard/NoFarms";
+
+// import main dashboard styles
+import styles from "../../styles/dashboard/index.module.css";
+
 const DashboardIndex: FunctionComponent = (): JSX.Element => {
     return (
-        <div style={{ height: "100vh" }}>
+        <div className={styles.mainDashboardPage}>
             <DashboardHeader />
-            <div>
-                <section>
-                    <DashboardMaincontrols />
+            <div className={styles.mainDashboardContainer}>
+                <section className={styles.mainControlSection}>
+                    <div className={styles.mainControlSectionElementContainer}>
+                        <DashboardMaincontrols />
+                    </div>
+                </section>
+                <section className={styles.mainControlSection}>
+                    <div className={styles.noFarmShower}>
+                        <NoFarms />
+                    </div>
+                </section>
+                <section className={styles.mainControlSection}>
+
                 </section>
             </div>
         </div>
