a35cfb82d5211c0b7329bca0e798dcd2138b9a07
JosiasAurel <ndjosiasaurel@gmail.com>
2021-07-11 16:24:15 +0100

    include jwt authorization for every request

--- packages/client/pages/_app.tsx
+++ packages/client/pages/_app.tsx
@@ -2,10 +2,24 @@ import React, { FunctionComponent } from "react";
 import { AppProps } from "next/app";
 
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import { ApolloLink } from "@apollo/client";
+
+const authLink: ApolloLink = setContext((_, { headers }) => {
+    const user = localStorage.getItem("user");
+
+    return {
+        headers: {
+            ...headers,
+            authorization: user ? `Bearer ${user}` : ""
+        }
+    }
+})
 
 const client = new ApolloClient({
     uri: "http://localhost:4000",
-    cache: new InMemoryCache()
+    cache: new InMemoryCache(),
+    link: authLink
 });
 
 const App: FunctionComponent<AppProps> = ({ Component, pageProps }): JSX.Element => {
