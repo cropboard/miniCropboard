0121930af722570c5cd8e1725c36a44bfa634d2d
JosiasAurel <ndjosiasaurel@gmail.com>
2021-07-11 13:12:00 +0100

    add apollo client and provider in _app

---
+++ packages/client/pages/_app.tsx
@@ -0,0 +1,19 @@
+import React, { FunctionComponent } from "react";
+import { AppProps } from "next/app";
+
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+const client = new ApolloClient({
+    uri: "http://localhost:4000",
+    cache: new InMemoryCache()
+});
+
+const App: FunctionComponent<AppProps> = ({ Component, pageProps }): JSX.Element => {
+    return (
+        <ApolloProvider client={client}>
+            <Component {...pageProps} />
+        </ApolloProvider>
+    )
+}
+
+export default App;
