a9c2e8619db4d5a6ed81c0fa327d6a02870fd59d
JosiasAurel <ndjosiasaurel@gmail.com>
2021-07-25 18:09:39 +0100

    update fetcher to provide name after signup

--- packages/client/utils/fetcher.ts
+++ packages/client/utils/fetcher.ts
@@ -2,14 +2,14 @@
 // load necessary .env
 
 // server uri
-const SERVER_URI: string = process.env.SERVER_URI;
+const SERVER_URI: string = process.env.CB_SERVER_URI;
 
 // headers
 const requestHeaders: HeadersInit = {
     "Content-Type": "application/json"
 };
 
-async function signupHandler(name: string, password: string, email: string, location: string) {
+async function signupHandler(server: string, name: string, password: string, email: string, location: string): Promise<any> {
 
     // data necessary to register new user
     let userData: object = {
@@ -20,15 +20,22 @@ async function signupHandler(name: string, password: string, email: string, loca
     };
 
     // fetch results
-    let fetchResult = await fetch(SERVER_URI, {
+    let fetchResult = await fetch(`${SERVER_URI || server}/signup`, {
+        method: "POST",
         headers: requestHeaders,
         body: JSON.stringify(userData)
     });
 
     // the data got back from server -> expecting success
-    let registrationResponse = await fetchResult.json();
+    let registrationResponse: any = await fetchResult.json();
 
     console.log(registrationResponse);
+
+    if (registrationResponse.Message === "NotCreated") {
+        return false;
+    }
+
+    return {state: "Created", token: registrationResponse.Token, name: registrationResponse.name}
 }
 
 export { signupHandler };
