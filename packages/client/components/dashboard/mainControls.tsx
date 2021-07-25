038de41f4634ca4cc1e805ac79e19d8f05c5de1f
JosiasAurel <ndjosiasaurel@gmail.com>
2021-07-25 20:47:19 +0100

    destroy mainControls

--- packages/client/components/dashboard/mainControls.tsx
+++ packages/client/components/dashboard/mainControls.tsx
@@ -1,18 +1,22 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useState, useEffect } from "react";
 
 // import mainControl components
 import CreateFarmButton from "./createFarmButton";
 import ControlButton from "./ControlButton";
 
-const DashboardMaincontrols: FunctionComponent = (): JSX.Element => {
-    return (
-        <aside>
-            <CreateFarmButton action={eval("console.log('yo')")} />
-            <ControlButton name="Analytics" iconSource="/dashboard/chart.svg" action={() => undefined} />
-            <ControlButton name="Tasks" iconSource="/dashboard/task.svg" action={() => undefined} />
-            <ControlButton name="History" iconSource="/dashboard/clock-outline.svg" action={() => undefined} />
-        </aside>
-    )
+// use modal
+import Modal from "../modal";
+
+co
+
+return (
+    <aside>
+        <CreateFarmButton action={function () { setModalOpen(!(modalOpen)) }} />
+        <ControlButton name="Analytics" iconSource="/dashboard/chart.svg" action={() => undefined} />
+        <ControlButton name="Tasks" iconSource="/dashboard/task.svg" action={() => undefined} />
+        <ControlButton name="History" iconSource="/dashboard/clock-outline.svg" action={() => undefined} />
+    </aside>
+)
 }
 
 export default DashboardMaincontrols;
