da26cebe7bd3f125491fd6fb749a25efaf7ccb5a
JosiasAurel <ndjosiasaurel@gmail.com>
2021-07-25 18:28:30 +0100

    add auth check

--- packages/client/pages/dashboard/index.tsx
+++ packages/client/pages/dashboard/index.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useState, useEffect } from "react";
 
 // import custom components
 import DashboardHeader from "../../components/dashboard/Header";
@@ -9,10 +9,51 @@ import NoFarms from "../../components/dashboard/NoFarms";
 // import main dashboard styles
 import styles from "../../styles/dashboard/index.module.css";
 
+interface userInfo {
+    userName: string
+    user: string // user jwt
+}
+
 const DashboardIndex: FunctionComponent = (): JSX.Element => {
+
+    // user information -> name and jwt
+    const [userInfo, setUserInfo] = useState<userInfo>();
+
+    // are you authenticated ?
+    const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+
+    // we want to check this before the component mounts
+    useEffect(() => {
+        // get jwt and name from localStorage
+        let name: string = localStorage.getItem("userName") ?? undefined;
+        let token: string = localStorage.getItem("user") ?? undefined;
+
+        console.log(name, token)
+
+        if (name === undefined || token === undefined) {
+            return
+        } else {
+            setUserInfo({ userName: name, user: token });
+            setIsAuthenticated(true);
+        }
+
+    }, [isAuthenticated]);
+
+    if (!isAuthenticated) {
+        return (
+            <div>
+                <DashboardHeader />
+                <div>
+                    <h2>You are not Authenticated</h2>
+                </div>
+            </div>
+        )
+    }
+
+
     return (
         <div className={styles.mainDashboardPage}>
-            <DashboardHeader />
+            <DashboardHeader name={userInfo.userName} />
             <div className={styles.mainDashboardContainer}>
                 <section className={styles.mainControlSection}>
                     <div className={styles.mainControlSectionElementContainer}>
