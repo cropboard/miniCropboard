b56076f7c18a8436c5df789195c69f61a440f449
JosiasAurel <ndjosiasaurel@gmail.com>
2021-07-25 19:10:10 +0100

    add createFarm fetcher

--- packages/client/utils/fetcher.ts
+++ packages/client/utils/fetcher.ts
@@ -40,7 +40,7 @@ async function signupHandler(server: string, name: string, password: string, ema
 
 
 // function to send GraphQL requests
-// query has to be JSON stringified
+// query has to be JSON stringified if JSON object
 // action can either be query or mutation
 async function sendGraphQLRequest(authToken: string, action: string, query: string): Promise<any> {
     // request headers for GraphQL request
@@ -77,6 +77,29 @@ async function sendGraphQLRequest(authToken: string, action: string, query: stri
     }
 
     
+}
+
+// function to create a farm
+// this is a mutation query
+async function createFarm(
+    authToken: string,
+    title: string, 
+    fertilizer: string, 
+    location: string, 
+    inputSeeds: string, 
+    plant: string, 
+    category: string): Promise<any> {
+
+    // the constructed graphql query with vars
+    const createFarmMutation: string = `
+        createFarm(title: ${title}, fertilizer: ${fertilizer}, location: ${location}, inputSeeds: ${inputSeeds}, plant: ${plant}, category: ${category}) {
+            title,
+            fertilizer
+        }
+    `;
+
+    let query = await sendGraphQLRequest(authToken, "mutation", createFarmMutation);
+
 }
 
 export { signupHandler };
