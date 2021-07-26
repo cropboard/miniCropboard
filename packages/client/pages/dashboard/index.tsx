e53266728647c2c8d53a4d12ae3a14c01270a1f5
JosiasAurel <ndjosiasaurel@gmail.com>
2021-07-26 17:33:03 +0100

    add createFarm form

--- packages/client/pages/dashboard/index.tsx
+++ packages/client/pages/dashboard/index.tsx
@@ -2,7 +2,6 @@ import React, { FunctionComponent, useState, useEffect } from "react";
 
 // import custom components
 import DashboardHeader from "../../components/dashboard/Header";
-import CreateFarmButton from "../../components/dashboard/createFarmButton";
 
 import NoFarms from "../../components/dashboard/NoFarms";
 import NotAuthCard from "../../components/dashboard/notAuthCard";
@@ -13,6 +12,9 @@ import styles_ from "../../styles/misc.module.css";
 
 // use modal
 import Modal from "../../components/modal";
+
+// import creatFarm handler
+import { createFarm } from "../../utils/fetcher";
 
 interface userInfo {
     userName: string
@@ -27,8 +29,34 @@ const DashboardIndex: FunctionComponent = (): JSX.Element => {
     // are you authenticated ?
     const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
 
+    /* Create Farm */
     // modals states
     const [farmCreateOpen, setFarmCreateOpen] = useState<boolean>(false);
+
+    // states for createFarm modal
+    const [title, setTitle] = useState<string>("");
+    const [plant, setPlant] = useState<string>("");
+    const [category, setCategory] = useState<string>("");
+    const [inputSeeds, setInputSeeds] = useState<string>("");
+    const [fertilizer, setFertilizer] = useState<string>("");
+    const [location, setLocation] = useState<string>("");
+
+    // handle input value change
+    function textFieldChangehandler(event: any, handler: Function): void {
+        handler(event.target.value);
+    }
+
+    async function submitCreateFarmForm(event: any): Promise<void> {
+        event.preventDefault();
+        if (title !== "" && plant !== "" && category !== "" && inputSeeds !== "" && fertilizer !== "" && location !== "") {
+            let results = await createFarm(userInfo.user, title, fertilizer, location, inputSeeds, plant, category);
+            return console.log(results);
+        } else {
+            alert("Some fields are empty");
+        }
+    }
+
+    /* End Create Farn */
 
     // we want to check this before the component mounts
     useEffect(() => {
@@ -36,7 +64,7 @@ const DashboardIndex: FunctionComponent = (): JSX.Element => {
         let name: string = localStorage.getItem("userName") ?? undefined;
         let token: string = localStorage.getItem("user") ?? undefined;
 
-        console.log(name, token)
+        console.log(name, token);
 
         if (name === undefined || token === undefined) {
             return
@@ -75,19 +103,19 @@ const DashboardIndex: FunctionComponent = (): JSX.Element => {
             <div className={styles.modalsContainer}>
                 <div className={styles_.modalContainer}>
                     <Modal modalState={farmCreateOpen} closeHandler={() => setFarmCreateOpen(!farmCreateOpen)}>
-                        <form className={styles.formStyle} style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
+                        <form onSubmit={event => submitCreateFarmForm(event)} className={styles.formStyle}>
                             {/* title */}
-                            <input type="text" name="title" id="title" placeholder="Title of farm" />
+                            <input value={title} onChange={event => textFieldChangehandler(event, setTitle)} type="text" name="title" id="title" placeholder="Title of farm" />
                             {/* plant */}
-                            <input type="text" name="plant" id="plant" placeholder="Name of plant e.g Tomato" />
+                            <input value={plant} onChange={event => textFieldChangehandler(event, setPlant)} type="text" name="plant" id="plant" placeholder="Name of plant e.g Tomato" />
                             {/* category */}
-                            <input type="text" name="category" id="category" placeholder="Type of plant e.g fruit, legume..." />
+                            <input value={category} onChange={event => textFieldChangehandler(event, setCategory)} type="text" name="category" id="category" placeholder="Type of plant e.g fruit, legume..." />
                             {/* inputSeeds */}
-                            <input type="text" name="inputSeeds" id="inputSeeds" placeholder="Quantity of seeds in kg or tonnes" />
+                            <input value={inputSeeds} onChange={event => textFieldChangehandler(event, setInputSeeds)} type="text" name="inputSeeds" id="inputSeeds" placeholder="Quantity of seeds in kg or tonnes" />
                             {/* fertilizer */}
-                            <input type="text" name="fertilizer" id="fertilizer" placeholder="Type of fertilizer e.g NPK" />
+                            <input value={fertilizer} onChange={event => textFieldChangehandler(event, setFertilizer)} type="text" name="fertilizer" id="fertilizer" placeholder="Type of fertilizer e.g NPK" />
                             {/* location */}
-                            <input type="text" name="location" id="location" placeholder="Location of farm" />
+                            <input value={location} onChange={event => textFieldChangehandler(event, setLocation)} type="text" name="location" id="location" placeholder="Location of farm" />
 
                             {/* Submit */}
                             <button>
