54542be1412aee74554f7e48a374884f174ecacf
JosiasAurel <ndjosiasaurel@gmail.com>
2021-07-26 20:05:25 +0100

    link for farm card and image on newFarmCard

--- packages/client/components/dashboard/FarmCard.tsx
+++ packages/client/components/dashboard/FarmCard.tsx
@@ -3,25 +3,32 @@ import React, { FunctionComponent } from "react";
 
 import styles from "../../styles/dashboard/components.module.css";
 
+import Link from "next/link";
+
 interface FarmCardProps {
     title: string
     location: string
     fertilizer: string
     plant: string
     category: string
+    id: string
 }
 
-const FarmCard: FunctionComponent<FarmCardProps> = ({ title, location, fertilizer, plant, category }): JSX.Element => {
+const FarmCard: FunctionComponent<FarmCardProps> = ({ title, location, fertilizer, plant, category, id }): JSX.Element => {
     return (
-        <div className={styles.farmCard}>
-            <h2> {title} </h2>
-            <div>
-                <p> {plant} </p>
-                <p> {category} </p>
-                <p> {fertilizer} </p>
+        <Link href={`/dashboard/farm/${id}`}>
+            <div className={styles.farmCard}>
+                <h2> {title} </h2>
+                <div className={styles.varWidth}>
+                    <span className={styles.plant__type}>
+                        <p> {plant} </p>
+                        <p> {category} </p>
+                    </span>
+                    <p className={styles.fertilizer}> Fertilizer : {fertilizer} </p>
+                </div>
+                <p className={styles.location}> {location} </p>
             </div>
-            <p> {location} </p>
-        </div>
+        </Link>
     )
 }
 
--- packages/client/components/dashboard/NewFarmCard.tsx
+++ packages/client/components/dashboard/NewFarmCard.tsx
@@ -7,15 +7,15 @@ import styles from "../../styles/dashboard/components.module.css";
 interface NewFarmCardProps {
     action: Function
 }
-const FarmCard: FunctionComponent<NewFarmCardProps> = ({ action }): JSX.Element => {
+const NewFarmCard: FunctionComponent<NewFarmCardProps> = ({ action }): JSX.Element => {
     return (
         <div onClick={() => action()} className={styles.newFarmCard}>
             <div>
-                <img src="/dashboard/plus-outline.png" alt="" />
+                <img src="/dashboard/plus-outline.png" alt="new-farm" />
                 <h2>New Farm</h2>
             </div>
         </div>
     )
 }
 
-export default FarmCard;
+export default NewFarmCard;
