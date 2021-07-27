e88df56f6e75237dc40be1c99ed2dd111f0957e4
JosiasAurel <ndjosiasaurel@gmail.com>
2021-07-27 18:38:51 +0100

    create new crop card

---
+++ packages/client/components/dashboard/NewCropCard.tsx
@@ -0,0 +1,21 @@
+
+import React, { FunctionComponent } from "react";
+
+import styles from "../../styles/dashboard/components.module.css";
+
+interface NewCropCardProps {
+    action: Function
+}
+
+const NewCropCard: FunctionComponent<NewCropCardProps> = ({ action }): JSX.Element => {
+    return (
+        <div onClick={() => action()} className={styles.newFarmCard}>
+            <div>
+                <img src="/dashboard/plus-outline.png" alt="new-farm" />
+                <h2>New Crop</h2>
+            </div>
+        </div>
+    )
+}
+
+export default NewCropCard;
