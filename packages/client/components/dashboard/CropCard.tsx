27760a699612e7e31ce89d744511c992db7b000f
JosiasAurel <ndjosiasaurel@gmail.com>
2021-07-27 18:38:19 +0100

    create crop card

---
+++ packages/client/components/dashboard/CropCard.tsx
@@ -0,0 +1,24 @@
+import React, { FunctionComponent } from "react";
+
+import styles from "../../styles/dashboard/components.module.css";
+
+interface CropCardProps {
+    name: string
+    category: string
+    fertilizer: string
+}
+
+const CropCard: FunctionComponent<CropCardProps> = ({ name, category, fertilizer }): JSX.Element => {
+    return (
+        <div className={styles.cropCard}>
+            <h2 className={styles.cropCardTitle}> {name} </h2>
+            <div>
+                <p className={styles.cropCardFruit}> Fruit </p>
+                <p className={styles.cropCardClass}> Class :  {category} </p>
+            </div>
+            <p className={styles.cropCardFertilizer}>Fertilizer : {fertilizer}</p>
+        </div>
+    )
+}
+
+export default CropCard;
