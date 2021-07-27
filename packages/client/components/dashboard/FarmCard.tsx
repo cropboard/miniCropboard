a3361d245c77565a577b7d506f7cd28c207bccf0
JosiasAurel <ndjosiasaurel@gmail.com>
2021-07-27 13:58:40 +0100

    match farmCard for new farmData

--- packages/client/components/dashboard/FarmCard.tsx
+++ packages/client/components/dashboard/FarmCard.tsx
@@ -8,25 +8,23 @@ import Link from "next/link";
 interface FarmCardProps {
     title: string
     location: string
-    fertilizer: string
-    plant: string
     category: string
+    kind: string
     id: string
 }
 
-const FarmCard: FunctionComponent<FarmCardProps> = ({ title, location, fertilizer, plant, category, id }): JSX.Element => {
+const FarmCard: FunctionComponent<FarmCardProps> = ({ title, location, kind, category, id }): JSX.Element => {
     return (
         <Link href={`/dashboard/farm/${id}`}>
             <div className={styles.farmCard}>
                 <h2> {title} </h2>
                 <div className={styles.varWidth}>
                     <span className={styles.plant__type}>
-                        <p> {plant} </p>
-                        <p> {category} </p>
+                        <p> Category :  {category} Crop </p>
                     </span>
-                    <p className={styles.fertilizer}> Fertilizer : {fertilizer} </p>
+                    <p className={styles.fertilizer}> Class : {kind} </p>
                 </div>
-                <p className={styles.location}> {location} </p>
+                <p className={styles.location}> Location : {location} </p>
             </div>
         </Link>
     )
