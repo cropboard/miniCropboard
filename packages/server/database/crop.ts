3063768ba68a1192b6255997190c0a65e14fd5d8
JosiasAurel <ndjosiasaurel@gmail.com>
2021-07-27 10:05:02 +0100

    remove fields from crop that goes to cropData

--- packages/server/database/crop.ts
+++ packages/server/database/crop.ts
@@ -1,27 +1,12 @@
 import { Schema, Document, model } from "mongoose";
 
 // naturals
-enum CropCategory { Legume, Fruit, Vegetable }
+enum CropCategory { Legume, Fruit, Cereal }
 
-// all of the weather data below are atmoshperic
-/* 
-Humidity defaults to %
-temperature defaults to °C
-visibility round field 8.0km
-we will get dew point from 
-*/
-interface WeatherData {
-    speed: string
-    pressure: string
-    humidity: number
-    UV: number
-    atmosphericTemperature: number
-}
 
 interface CBCrop extends Document {
     name: string
     category: CropCategory
-    weather: WeatherData
     fertilizer: string
     water: string
     cost: string
@@ -32,7 +17,6 @@ interface CBCrop extends Document {
 const CropSchema = new Schema<CBCrop>({
     name: String,
     category: String,
-    weather: Object,
     fertilizer: String,
     water: String,
     cost: String,
