511fadd8aff53d151d559b2c760b670cff24bc37
JosiasAurel <ndjosiasaurel@gmail.com>
2021-07-27 10:05:52 +0100

    create crop data model

---
+++ packages/server/database/cropData.ts
@@ -0,0 +1,42 @@
+import { Document, model, Schema } from "mongoose";
+
+// all of the weather data below are atmoshperic
+/* 
+Humidity defaults to %
+temperature defaults to °C
+visibility round field 8.0km
+we will get dew point from 
+*/
+interface WeatherData {
+    speed: string
+    pressure: string
+    humidity: number
+    UV: number
+    atmosphericTemperature: number
+}
+
+interface  CBCropData extends Document {
+    name: string
+    category: string
+    fertilizer: string
+    fertilizerQuantity: number
+    water: number
+    cost: number
+    timeStamp: string
+    weather: WeatherData
+}
+
+const CropDataSchema = new Schema<CBCropData>({
+    name: String,
+    category: String,
+    fertilizer: String,
+    fertilizerQuantity: Number,
+    water: Number,
+    cost: Number,
+    timeStamp: String,
+    weather: Object
+});
+
+const CropData = model("CropData", CropDataSchema);
+
+export { CropData };
